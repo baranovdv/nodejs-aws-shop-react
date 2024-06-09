545bdb668c4e4c33c1373241eb3a05e3f97afe36
baranovdv <baranov.d.v.1988@gmail.com>
2024-06-10 00:28:40 +0500

    feat: implement deploy to s3

--- AWS-CDK/bin/cd_kexample.ts
+++ AWS-CDK/bin/cd_kexample.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdKexampleStack } from '../lib/cd_kexample-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { CdKexampleStack } from "../lib/cd_kexample-stack";
 
 const app = new cdk.App();
-new CdKexampleStack(app, 'CdKexampleStack', {
+new CdKexampleStack(app, "CdKexampleStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,7 @@ new CdKexampleStack(app, 'CdKexampleStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: '851725533394', region: 'eu-central-1' },
+  env: { account: "851725533394", region: "eu-central-1" },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- AWS-CDK/lib/cd_kexample-stack.ts
+++ AWS-CDK/lib/cd_kexample-stack.ts
@@ -1,21 +1,23 @@
-import * as cdk from 'aws-cdk-lib';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { Construct } from "constructs";
 
 export class CdKexampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const myS3bucket = new Bucket(this, 'MyS3BucketonSDK', {
-      bucketName: 'thisistestbucketname',
-      versioned: true
-    })
-    // The code that defines your stack goes here
+    const myS3bucket = new Bucket(this, "MyS3BucketonSDK", {
+      bucketName: "aws-dev-course-shop-cdk",
+      versioned: true,
+      publicReadAccess: true,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
+      websiteIndexDocument: "index.html",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdKexampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new BucketDeployment(this, "DeployWebsite", {
+      sources: [Source.asset("../dist")],
+      destinationBucket: myS3bucket,
+    });
   }
 }
