7db01ce806641c18b094985994b24606fb0b373b
baranovdv <baranov.d.v.1988@gmail.com>
2024-06-10 01:37:56 +0500

    feat: implement cloudfront

--- AWS-CDK/lib/cd_kexample-stack.ts
+++ AWS-CDK/lib/cd_kexample-stack.ts
@@ -1,23 +1,31 @@
 import * as cdk from "aws-cdk-lib";
-import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3";
-import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 import { Construct } from "constructs";
 
 export class CdKexampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const myS3bucket = new Bucket(this, "MyS3BucketonSDK", {
+    const myS3bucket = new s3.Bucket(this, "MyS3BucketonSDK", {
       bucketName: "aws-dev-course-shop-cdk",
       versioned: true,
       publicReadAccess: true,
-      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
       websiteIndexDocument: "index.html",
     });
 
-    new BucketDeployment(this, "DeployWebsite", {
-      sources: [Source.asset("../dist")],
+    const distribution = new cloudfront.Distribution(this, "MyDistribution", {
+      defaultBehavior: { origin: new origins.S3Origin(myS3bucket) },
+    });
+
+    new s3deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [s3deploy.Source.asset("../dist")],
       destinationBucket: myS3bucket,
+      distribution,
+      distributionPaths: ["/*"],
     });
   }
 }
