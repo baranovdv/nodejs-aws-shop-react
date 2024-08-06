b2722cc59e46e067d7acbd8ec9a45558d456ad71
baranovdv <baranov.d.v.1988@gmail.com>
2024-08-06 16:23:09 +0500

    feat: update cart endpoint to eb endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://8yihah1o06.execute-api.ap-southeast-2.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://lp7ms5mbwj.execute-api.ap-southeast-2.amazonaws.com/prod/api",
+  cart: "http://baranovdv-cart-api-dev.ap-southeast-2.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
